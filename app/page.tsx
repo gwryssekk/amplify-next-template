b0e8074a0735cc108ca9a26432733ebd89539e58
gwryssekk <gwrussell@gmail.com>
2025-02-23 13:56:03 -0800

    trying again

--- app/page.tsx
+++ app/page.tsx
@@ -1,73 +1,24 @@
 'use client'
-import { Authenticator } from "@aws-amplify/ui-react";
-import { AIConversation } from '@aws-amplify/ui-react-ai';
-import { useAIConversation, useAIGeneration } from "./client";
-import { useAuthenticator } from "@aws-amplify/ui-react";
-import {
-  Button,
-  Flex,
-  Heading,
-  Loader,
-  Text,
-  TextAreaField,
-  View,
-} from "@aws-amplify/ui-react";
-import React from "react";
-export default function Page() {
+import { generateClient } from "aws-amplify/data";
+import { createAIHooks, AIConversation } from '@aws-amplify/ui-react-ai';
+import type { Schema } from "../amplify/data/resource";
+
+const client = generateClient<Schema>({ authMode: 'userPool' });
+const { useAIConversation } = createAIHooks(client);
+
+export default function Chat() {
   const [
     {
       data: { messages },
-      isLoading,
     },
-    handleSendMessage,
+    sendMessage,
   ] = useAIConversation('chat');
-  // 'chat' is based on the key for the conversation route in your schema.
-  const { signOut } = useAuthenticator();
-  const [description, setDescription] = React.useState("");
-  const [{ data, hasError }, generateRecipe] =
-    useAIGeneration("generateRecipe");
-
-  const handleClick = () => {
-    generateRecipe({ description });
-  };
+  // 'chat' here should be the key in your schema
 
   return (
-    <>
-      <AIConversation
-        messages={messages}
-        isLoading={isLoading}
-        handleSendMessage={handleSendMessage}
-      />
-       <Flex direction="column">
-      <Flex direction="row">
-        <TextAreaField
-          autoResize
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          label="Description"
+        <AIConversation
+            messages={messages}
+            handleSendMessage={sendMessage}
         />
-        <Button onClick={handleClick}>Generate Suggestion</Button>
-      </Flex>
-      {isLoading ? (
-        <Loader variation="linear" />
-      ) : (
-        <>
-          <Heading level={2}>{data?.name}</Heading>
-          <View as="ul">
-            {data?.ingredients?.map((ingredient) => (
-              <Text as="li" key={ingredient}>
-                {ingredient}
-              </Text>
-            ))}
-          </View>
-          <Text>{data?.instructions}</Text>
-        </>
-      )}
-    </Flex>
-      <br />
-    <button onClick={signOut}>Sign out</button>
-    
-    </>
-  );
-
+    )
 }
