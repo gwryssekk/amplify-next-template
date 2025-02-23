1b9ef629dcea55f7d14db2f1858aa0ebdc3101e3
gwryssekk <gwrussell@gmail.com>
2025-02-23 14:52:45 -0800

    more

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -4,7 +4,7 @@ const schema = a.schema({
   chat: a.conversation({
     aiModel: a.ai.model("Claude 3.5 Haiku"),
     systemPrompt:
-      "You are a travel advisor app. You help travelers find locations.",
+      `You are advising students who want to know what the best career path for them is.  Ask a series of questions (no more than 5), and when that is complete provide the student with a list of the 3 best career fields.  Generate a fit score, where 100 is the best fist, and 0 is the worst fit, and tell them the fit score for each career choice`,
   }).authorization(allow => allow.owner()),
 });
 
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import { generateClient } from "aws-amplify/data";
 import { createAIHooks, AIConversation } from '@aws-amplify/ui-react-ai';
 import type { Schema } from "../amplify/data/resource";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 
 const client = generateClient<Schema>({ authMode: 'userPool' });
 const { useAIConversation } = createAIHooks(client);
@@ -15,10 +16,23 @@ export default function page() {
   ] = useAIConversation('chat');
   // 'chat' here should be the key in your schema
 
+  const { user, signOut } = useAuthenticator();
   return (
+    <>
+        <p>Hello {user.username}</p>
+        <br />
+        <p>
         <AIConversation
             messages={messages}
             handleSendMessage={sendMessage}
         />
-    )
+        </p>
+        <br />
+        <p >
+            <button onClick={signOut} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sign Out</button>
+        </p>
+      
+    
+    </>
+  )
 }
