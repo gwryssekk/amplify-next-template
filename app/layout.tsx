ea8a9a5077ebf9f6198dd569ee7b057d2cf4e9de
gwryssekk <gwrussell@gmail.com>
2025-02-23 18:01:47 -0800

    removing default styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import { Amplify } from "aws-amplify";
-import "./app.css";
 import { Authenticator, ThemeProvider, defaultDarkModeOverride } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import outputs from "@/amplify_outputs.json";
